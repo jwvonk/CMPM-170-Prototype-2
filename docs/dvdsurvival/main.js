244e867973ad355f3322f2ed745ed6fbb6ca3690
ofcourseitsgv <84610338+ofcourseitsgv@users.noreply.github.com>
2023-11-16 12:52:13 -0800

    Decrease enemy size, increase enemy count

--- docs/dvdsurvival/main.js
+++ docs/dvdsurvival/main.js
@@ -4,28 +4,29 @@ description = `Avoid the enemies!
 `;
 
 characters = [
-`
+  `
 ll ll
 l ll l
 ll ll
-`	
+`,
 ];
 
 const G = {
 	WIDTH: 150,
 	HEIGHT: 100,
 	ENEMY_SPEED: 1,
+	ENEMY_SIZE: 8,
+	ENEMY_MAX: 7,
 	PLAYER_BASE_SPEED: 1,
 	PLAYER_WIDTH: 6,
-	PLAYER_HEIGHT: 4
+	PLAYER_HEIGHT: 4,
 };
 
 options = {
-	viewSize: {x: G.WIDTH, y: G.HEIGHT},
+	viewSize: { x: G.WIDTH, y: G.HEIGHT },
 	theme: "dark",
-	isReplayEnabled: true
+	isReplayEnabled: true,
 };
-
 
 /**
  * @typedef {{
@@ -41,12 +42,11 @@ options = {
  */
 let player;
 
-
 /**
  * @typedef {{
-* pos: Vector
-* }} Enemy
-*/
+ * pos: Vector
+ * }} Enemy
+ */
 
 /**
  * @type { Enemy [] }
@@ -58,72 +58,70 @@ let enemies;
  */
 let colorCount = 0;
 
-function colorShift(t){
-	switch(t){
+function colorShift(t) {
+	switch (t) {
 		case 0:
-			color("red");
-			break;
+		color("red");
+		break;
 		case 1:
-			color("yellow");
-			break;
+		color("yellow");
+		break;
 		case 2:
-			color("green");
-			break;
+		color("green");
+		break;
 		case 3:
-			color("cyan");
-			break;
+		color("cyan");
+		break;
 		case 4:
-			color("blue");
-			break;
+		color("blue");
+		break;
 		case 5:
-			color("purple");
-			break;
+		color("purple");
+		break;
 		default:
-			color("red");
-			break;
+		color("red");
+		break;
 	}
 }
 
 function update() {
 	if (!ticks) {
 		player = {
-			pos: vec(G.WIDTH * 0.5, G.HEIGHT * 0.5),
-			speed: G.PLAYER_BASE_SPEED,
-			isMovingLeft: false,
-			isMovingUp: false
+		pos: vec(G.WIDTH * 0.5, G.HEIGHT * 0.5),
+		speed: G.PLAYER_BASE_SPEED,
+		isMovingLeft: false,
+		isMovingUp: false,
 		};
 
 		enemies = [];
 	}
 	
-	if (enemies.length === 0) {
-		for (let i = 0; i < 4; i++) {
-			const posX = rnd(0, G.WIDTH);
-			const posY = -i * G.HEIGHT * 0.1;
-			enemies.push({
-				pos: vec(posX, posY),
-			 })
-		}
+	if (enemies.length < G.ENEMY_MAX) {
+		const posX = rnd(0, G.WIDTH);
+		const posY = -1 * (G.HEIGHT + rnd(-G.HEIGHT, G.HEIGHT/ 2));
+		enemies.push({
+			pos: vec(posX, posY),
+		});
 	}
 
 	if (input.isJustPressed) {
 		player.isMovingUp = !player.isMovingUp;
 	}
 
-	if (player.pos.x >= G.WIDTH - G.PLAYER_WIDTH/2 || player.pos.x <= G.PLAYER_HEIGHT/2) {
+	if (player.pos.x >= G.WIDTH - G.PLAYER_WIDTH / 2 || player.pos.x <= G.PLAYER_HEIGHT / 2) {
 		player.isMovingLeft = !player.isMovingLeft;
 
 		addScore(1, player.pos);
 		play("coin");
 		colorCount++;
 
-		// Check if the score is less than 100 before incrementing speed
+		// increase speed each 10 score (and below 100 score)
 		if (score < 100 && score % 10 === 0) {
 			player.speed += 0.1;
 		}
 	}
 
-	 if (player.pos.y >= G.HEIGHT - G.PLAYER_HEIGHT/2 || player.pos.y <= G.PLAYER_HEIGHT/2) {
+	if (player.pos.y >= G.HEIGHT - G.PLAYER_HEIGHT / 2 || player.pos.y <= G.PLAYER_HEIGHT / 2) {
 		player.isMovingUp = !player.isMovingUp;
 
 		addScore(1, player.pos);
@@ -135,11 +133,10 @@ function update() {
 		}
 	}
 
-	player.pos.x += (player.isMovingLeft) ? -player.speed : player.speed;
-	player.pos.y += (player.isMovingUp) ? -player.speed : player.speed;
+	player.pos.x += player.isMovingLeft ? -player.speed : player.speed;
+	player.pos.y += player.isMovingUp ? -player.speed : player.speed;
 
-	// player.pos.clamp(0, G.WIDTH, 0, G.HEIGHT);
-	colorShift(colorCount%6);
+	colorShift(colorCount % 6);
 	char("a", player.pos);
 
 	remove(enemies, (e) => {
@@ -147,12 +144,12 @@ function update() {
 
 		color("black");
 
-		const isCollidingWithPlayer = box(e.pos, 10).isColliding.char.a;
+		const isCollidingWithPlayer = box(e.pos, G.ENEMY_SIZE).isColliding.char.a;
 
-		if (isCollidingWithPlayer){
-		play('hit');
+		if (isCollidingWithPlayer) {
+		play("hit");
 		end();
 		}
-		return (e.pos.y > G.HEIGHT);
+		return e.pos.y > (G.HEIGHT + G.ENEMY_SIZE);
 	});
 }
